b4867b8b01e5cb4a60ed8259dc8aa2974a43bd9e
mochi5o <1610.baumkuchen.124@gmail.com>
2021-05-20 20:29:35 +0900

    fix index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,16 +7,16 @@ async function run() {
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
 
-    const octokit = github.getOctokit(token);
+    const octokit = new github.getOctokit(token);
 
     const newIssue = await octokit.issues.create({
-        repo: github.context.repo.repo,
-        owner: github.context.repo.owner,
-        title: issueTitle,
-        body: jokeBody
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      title: issueTitle,
+      body: jokeBody
     });
-  } catch (err) {
-      core.setFailed(err.message);
+  } catch (error) {
+    core.setFailed(error.message);
   }
 }
 
